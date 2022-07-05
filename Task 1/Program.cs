c19c5b70c59042eee7359d4e55f4d8137c19d2bb
finall2022 <finall.git2022@gmail.com>
2022-07-05 21:20:50 +0300

    Решена 1 задача

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,29 +1,47 @@
 ﻿/* Задача 1: Задайте двумерный массив. 
 Напишите программу, которая упорядочит по убыванию элементы 
 каждой строки двумерного массива. */
-int Sort(arrayTemp[,])
-{
-    for (int n = 0; n < arrayTemp.Length / 2; n++)
-    {
-        if (arrayTemp[n, m])
-        {
 
-        }
-    }
-}
 Random rndRows = new Random();
 Random rndColumns = new Random();
-int rows = rndRows.Next(2, 3);
+int rows = rndRows.Next(2, 10);
 int columns = rndColumns.Next(2, 10);
 int[,] array = new int[rows, columns];
 Random rand = new Random();
+Console.WriteLine("Массив до сортировки:");
 for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < columns; j++)
     {
-        array[i, j] = rand.Next(10, 1000);
+        array[i, j] = rand.Next(10, 100);
         Console.Write($"{array[i, j]} \t");
     }
     Console.WriteLine();
 }
-int Sort(array[]);
+
+for (int k = 0; k <= (rows * columns); k++)
+{
+    for (int m = 0; m < rows; m++)
+    {
+        for (int n = 0; n < columns - 1; n++)
+        {
+            if (array[m, n] < array[m, n + 1])
+            {
+                int temp = array[m, n];
+                array[m, n] = array[m, n + 1];
+                array[m, n + 1] = temp;
+            }
+        }
+    }
+
+}
+Console.WriteLine("После сортировки строк по убыванию:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+
+}
